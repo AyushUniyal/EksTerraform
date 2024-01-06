fc6d13e7e2a52b393738c5aec4441af8cc3de935
AyushUniyal <ec2-user@ip-172-31-1-64.ap-south-1.compute.internal>
2024-01-06 16:19:50 +0000

    modified deployement.yml and service.yml

--- JenkinsServer/Install_Jenkins.sh
+++ JenkinsServer/Install_Jenkins.sh
@@ -21,4 +21,4 @@ sudo yum -y install terraform
 #finally install kubect1
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
-sudo mkdir -p SHOME/bin && sudo cp ./kubectl SHOME/bin/kubectl && export PATH=$PATH: $HOME/bin
+sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
